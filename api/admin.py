597dbe433688597b8969cc91a1c21fe95f4ce368
iammwirigi <mwirigimburugu81@gmail.com>
2025-06-19 11:27:32 +0300

    setup

--- api/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import YourModelName  # Replace with your actual model name
-
-admin.site.register(YourModelName)  # Register your model with the admin site
--- api/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path('example/', views.example_view, name='example_view'),
-]
